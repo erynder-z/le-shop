edcf9d11e5c843347445672533ab258676815983
erynder-z <stfn.bgr@gmail.com>
2022-05-04 11:34:45 +0200

    Setup dom-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import Nav from "./components/Nav";
+import Home from "./components/Home";
+import Shop from "./components/Shop";
+import Contact from "./components/Contact";
 
 function App() {
-  return <div>hello</div>;
+  <div className="app">
+    <Nav />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/search" element={<Shop />} />
+      <Route path="/about" element={<Contact />} />
+    </Routes>
+  </div>;
 }
 
 export default App;
