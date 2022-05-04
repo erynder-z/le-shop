024802cfde1933e1f1bdb7381b4e98bd373d081a
erynder-z <stfn.bgr@gmail.com>
2022-05-04 15:46:22 +0200

    Fetch fake products list from fake store API

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Shop.css";
 
 function Shop() {
-  return <div className="shop">Shop</div>;
+  const [products, setProducts] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+
+  async function fetchProducts() {
+    setIsFetching(true);
+    try {
+      const response = await fetch(`https://fakestoreapi.com/products`);
+
+      const fetchData = await response.json();
+      setProducts(() => fetchData);
+      setIsFetching(false);
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  return <div className="shop" />;
 }
 
 export default Shop;
