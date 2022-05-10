1924d0010c85b5146cda4f3d49b20b2974fa5002
erynder-z <stfn.bgr@gmail.com>
2022-05-10 15:06:39 +0200

    Add home component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import video from "../../assets/backvid.mp4";
 import "./Home.css";
 
 function Home() {
-  return <div className="home">Home</div>;
+  return (
+    <div className="home">
+      <video className="background.video" autoPlay muted loop id="backvid">
+        <source src={video} type="video/mp4" />
+      </video>
+      <div className="content">
+        <div className="content-left">
+          <h1>Welcome to Le Shop!</h1>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod,
+            necessitatibus?
+          </p>
+        </div>
+        <div className="content-right">
+          <div className="shopBtn">
+            <Link to="/shop">Explore the shop</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
